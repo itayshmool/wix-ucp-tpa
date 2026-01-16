6c1e4ef3edf1f91e3a0a1686010e325ad5e3c73c
itays <itays@wix.com>
2026-01-16 20:55:55 +0200

    chore: Remove test files

--- test-checkout-methods.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Test script to discover checkout methods
- */
-
-import { checkout } from '@wix/ecom';
-
-console.log('Checkout module methods:');
-console.log(Object.keys(checkout).filter(k => typeof (checkout as any)[k] === 'function'));
